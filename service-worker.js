ca823737a0fafa27691133d375f3fb354f02ebd4
Arya Sarukkai <66439562+aryasarukkai@users.noreply.github.com>
2024-03-09 23:33:35 -0800

    pwa support

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
-const cacheName = 'scouting-app-v1';
-const filesToCache = [
-    '/',
-    '/index.html',
-    '/logo.png'
-];
-
-self.addEventListener('install', event => {
-    console.log('Service Worker installing...');
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => cache.addAll(filesToCache))
-    );
-});
-
-self.addEventListener('fetch', event => {
-    console.log('Service Worker fetching...');
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+	  caches.open('scouting-app-cache').then(function(cache) {
+		return cache.addAll([
+		  '/',
+		  '/index.html',
+		  '/manifest.json',
+		  '/icon.png'
+		]);
+	  })
+	);
+  });

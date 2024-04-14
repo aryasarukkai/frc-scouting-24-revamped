b635166a3cd094648068e5b17b60708b8273bbfe
ad0gg0 <anirwinkotti53@gmail.com>
2024-04-14 11:51:40 -0700

    succesful slack messages with slack bot

--- scouting-app/bot.py
+++ scouting-app/bot.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 env_path = Path(".") / '.env'
-os.environ['SLACK_TOKEN'] = 'xoxb-1286819291120-6955692873013-MnhV2rLaFePl097kRkJcr0A5'
+os.environ['SLACK_TOKEN'] = 'xoxb-1286819291120-6911629796257-Nw9KlsmlD8wMNy78wCJf4OrG'
 load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#match_updates',text="Hello no it going")
+client.chat_postMessage(channel='#match_updates',text="test2")
